2a36c3a42ff917dc67e01e13b83c578ea9917a81
Ulubilge Ulusoy <ulubilgeulusoy@gmail.com>
2026-04-07 11:29:25 -0600

    camera index changed

--- openface_realtime_lsl.py
+++ openface_realtime_lsl.py
@@ -190,7 +190,7 @@ def main():
     landmark_detector = LandmarkDetector(model_path=landmark_model_path, device=device)
     multitask_model = MultitaskPredictor(model_path=multitask_model_path, device=device)
 
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         return
